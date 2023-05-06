8821645472bc74c0fcb9586e1cf71e98c2b582a9
yukichi6105 <107849799+YUKICHI6105@users.noreply.github.com>
2023-05-06 15:24:21 +0900

    初期化

---
+++ include/can_utils.hpp
@@ -0,0 +1,30 @@
+#include <array>
+#include <can_plugins2/msg/frame.hpp>
+#define CAN_MTU 8
+
+
+template <typename T>
+static void can_unpack(const std::array<uint8_t, CAN_MTU> &buf, T &data)
+{
+  memcpy(&data, buf.data(), sizeof(T));
+}
+
+template<typename T>
+static void can_pack(std::array<uint8_t, CAN_MTU> &buf, const T data)
+{
+  memcpy(buf.data(), &data, sizeof(T));
+}
+
+template<typename T>
+static std::unique_ptr<can_plugins2::msg::Frame> get_frame(const uint16_t id, const T data)
+{
+  auto frame = std::make_unique<can_plugins2::msg::Frame>();
+  frame->id = id;
+  frame->is_rtr = false;
+  frame->is_extended = false;
+  frame->is_error = false;
+  frame->dlc = sizeof(T);
+  can_pack<T>(frame->data, data);
+
+  return frame;
+}
---
+++ src/steering4.cpp
@@ -0,0 +1,110 @@
+#include <memory>
+#include <string>
+
+#include <rclcpp/rclcpp.hpp>
+#include "can_plugins2/msg/frame.hpp"
+#include <sensor_msgs/msg/joy.hpp>
+#include "../include/can_utils.hpp"
+
+
+using std::placeholders::_1;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+* member function as a callback from the timer. */
+
+struct BIDNumber
+{
+  uint32_t solenoidValve      = 0x100;
+  uint32_t upperRightwheel    = 0x110;
+  uint32_t upperRightsteering = 0x210;
+  uint32_t upperLeftwheel     = 0x120;
+  uint32_t upperLeftsteering  = 0x220;
+  uint32_t lowerRightwheel    = 0x130;
+  uint32_t lowerRightsteering = 0x230;
+  uint32_t lowerLeftwheel     = 0x140;
+  uint32_t lowerLeftsteering  = 0x240;
+};
+
+class pubsub : public rclcpp::Node
+{
+  public:
+    pubsub() : Node("steering4_node"), count_(0)
+    {
+      publisher_ = this->create_publisher<can_plugins2::msg::Frame>("can_tx", 10);
+      subscriber_ = this->create_subscription<sensor_msgs::msg::Joy>("joy", 10, std::bind(&pubsub::joy_callback, this, _1));
+    }
+
+  private:
+    BIDNumber bidNumber;
+    void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg);
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<can_plugins2::msg::Frame>::SharedPtr publisher_;
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscriber_;
+    size_t count_;
+};
+
+void pubsub::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
+  {  
+    RCLCPP_INFO(this->get_logger(), "I heard:");
+
+    if(msg->buttons[2]==true)
+    {
+      publisher_->publish(get_frame(pubsub::bidNumber.upperLeftwheel,static_cast<uint8_t>(5)));
+      publisher_->publish(get_frame(pubsub::bidNumber.upperRightwheel,static_cast<uint8_t>(5)));
+      publisher_->publish(get_frame(pubsub::bidNumber.lowerLeftwheel,static_cast<uint8_t>(5)));
+      publisher_->publish(get_frame(pubsub::bidNumber.lowerRightwheel,static_cast<uint8_t>(5)));
+      publisher_->publish(get_frame(pubsub::bidNumber.upperLeftsteering,static_cast<uint8_t>(5)));
+      publisher_->publish(get_frame(pubsub::bidNumber.upperRightsteering,static_cast<uint8_t>(5)));
+      publisher_->publish(get_frame(pubsub::bidNumber.lowerLeftsteering,static_cast<uint8_t>(5)));
+      publisher_->publish(get_frame(pubsub::bidNumber.lowerRightsteering,static_cast<uint8_t>(5)));
+    }
+
+    if(msg->buttons[1]==true)
+    {
+      publisher_->publish(get_frame(pubsub::bidNumber.upperLeftwheel,static_cast<uint8_t>(0)));
+      publisher_->publish(get_frame(pubsub::bidNumber.upperRightwheel,static_cast<uint8_t>(0)));
+      publisher_->publish(get_frame(pubsub::bidNumber.lowerLeftwheel,static_cast<uint8_t>(0)));
+      publisher_->publish(get_frame(pubsub::bidNumber.lowerRightwheel,static_cast<uint8_t>(0)));
+      publisher_->publish(get_frame(pubsub::bidNumber.upperLeftsteering,static_cast<uint8_t>(0)));
+      publisher_->publish(get_frame(pubsub::bidNumber.upperRightsteering,static_cast<uint8_t>(0)));
+      publisher_->publish(get_frame(pubsub::bidNumber.lowerLeftsteering,static_cast<uint8_t>(0)));
+      publisher_->publish(get_frame(pubsub::bidNumber.lowerRightsteering,static_cast<uint8_t>(0)));
+    }
+
+    float x= -(msg->axes[0]);
+    float y=  (msg->axes[1]);
+/*    float r= 0;
+    if(msg->buttons[4]==true){
+      r =1.0f;
+    }
+    //↑左回転
+    else if(msg->buttons[5]==true){
+      r =-1.0f;
+    }
+    //右回転
+    else if(msg->buttons[4] == msg->buttons[5]){
+      r =0.0f;
+    }
+*/    
+    publisher_->publish(get_frame((pubsub::bidNumber.upperRightwheel+1), 6.28*(x*x+y*y)));
+    publisher_->publish(get_frame((pubsub::bidNumber.upperLeftwheel+1), 6.28f*(x*x+y*y)));
+    publisher_->publish(get_frame((pubsub::bidNumber.lowerLeftwheel+1), 6.28f*(x*x+y*y)));
+    publisher_->publish(get_frame((pubsub::bidNumber.lowerRightwheel+1), 6.28f*(x*x+y*y)));
+    //wheel速度制御
+
+    publisher_->publish(get_frame((pubsub::bidNumber.upperRightsteering+1), 0));
+    publisher_->publish(get_frame((pubsub::bidNumber.upperLeftsteering+1), 0));
+    publisher_->publish(get_frame((pubsub::bidNumber.lowerLeftsteering+1), 0));
+    publisher_->publish(get_frame((pubsub::bidNumber.lowerRightsteering+1), 0));
+    //steering制御
+
+    RCLCPP_INFO(this->get_logger(), "Publishing:bokuha warukunai!");
+  }
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<pubsub>());
+  rclcpp::shutdown();
+  return 0;
+}
